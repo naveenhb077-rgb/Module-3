5547cdecda2887a618f0fccb5f41c76c0e38b707
naveenhb077-rgb <naveenhb077@gmail.com>
2025-12-01 18:06:19 +0530

    Create Garbage collection+finalize.java

---
+++ Garbage collection+finalize.java
@@ -0,0 +1,32 @@
+class FinalizeExample {
+    private int num;
+
+    public FinalizeExample(int n) {
+        num = n;
+        System.out.println("Object " + num + " created");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("finalize() called for object " + num);
+        super.finalize();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Creating objects...");
+        FinalizeExample obj1 = new FinalizeExample(1);
+        FinalizeExample obj2 = new FinalizeExample(2);
+
+        System.out.println("Making objects eligible for GC...");
+        obj1 = null;
+        obj2 = null;
+
+        System.gc();
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {}
+
+        System.out.println("End of main method");
+    }
+}
