a092e1f6ac30d09f359b214f76695375b67e9db1
naveenhb077-rgb <naveenhb077@gmail.com>
2025-12-01 18:00:59 +0530

    Create Simple class and object creation.java

---
+++ Simple class and object creation.java
@@ -0,0 +1,15 @@
+public class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Kishan";
+        s1.age = 21;
+        s1.display();
+    }
+}
