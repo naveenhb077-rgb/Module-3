8146a9dab690379b181c022361d783468d1efa7d
naveenhb077-rgb <naveenhb077@gmail.com>
2025-12-01 18:01:48 +0530

    Create Constructors.java

---
+++ Constructors.java
@@ -0,0 +1,18 @@
+public class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Rakesh", 23);
+        s1.display();
+    }
+}
